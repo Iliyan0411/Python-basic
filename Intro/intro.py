7936e5f62f47b280a2437dc833f2e54ba3dfe125
Iliyan0411 <ilian_dd00@abv.bg>
2021-01-02 16:09:36 +0200

    intro

---
+++ Intro/intro.py
@@ -0,0 +1,67 @@
+# surface of rectangle
+# a = int(input())
+# b = int(input())
+
+# S = a * b
+# print('Surface = ', S)
+######################
+
+# for-loop from 1 to 10
+# for x in range(1, 11):
+#     print(x)
+######################
+
+# input number with text
+# num = int(input('Enter number: '))
+# print(num)
+######################
+
+# string input
+# name = input('Enter your name: ')
+# print('Hello,', name, end='!')
+######################
+
+# printing with template PLACEHOLDER   
+# fstName = input()
+# lstName = input()
+# age = int(input())
+# town = input()
+
+# print('\nYou are {0} {1}, a {2}-years student from {3}'.format(fstName,lstName,age,town), end='.')
+######################
+
+# raising the degree
+# print(2 ** 5)
+######################
+
+# concat number and string
+# fstName = 'Iliyan'
+# lstName = 'Dimitrov'
+# age = 20
+
+# string = fstName + ' ' + lstName + ', ' + str(age) + ' years.'
+# print(string)
+######################
+
+# repeat text
+# expr = 'hi '
+# print(4 * expr)
+######################
+
+# formating strings
+# %d - integer
+# %f - real number
+# %s - string
+
+# width = 5
+# height = 4
+# text = 'area'
+
+# print('width = %d; height = %d; %s = %d;' % (width, height, text, width*height), end=' ')
+######################
+
+# other important symbols
+# \n - new line
+# \b = delete last symbol
+
+# print('ab\bc')
