862c0995e8af8b1ae55f7cbb453d9b4d27b7e66a
Iliyan0411 <ilian_dd00@abv.bg>
2021-04-02 18:09:49 +0300

    .

---
+++ solution.py
@@ -0,0 +1,51 @@
+
+def raft(K, weights):
+    min = max(weights)
+
+    groups = []
+    for i in range(0, K):
+        groups.append([])
+
+    gc = 0
+    i = 0
+    while i < len(weights):
+        if weights[i] + sum(groups[gc]) <= min:
+            groups[gc].append(weights[i])
+            i += 1
+        else:
+            if gc < K - 1:
+                gc += 1
+            else:
+                minIndex = 0
+                for j in range(1, K):
+                    if sum(groups[j]) < sum(groups[minIndex]):
+                        minIndex = j
+                
+                groups[minIndex].append(weights[i])
+                min = sum(groups[minIndex])
+                gc = 0
+                i += 1
+
+    maxIndex = 0
+    for j in range(1, K):
+        if sum(groups[j]) > sum(groups[maxIndex]):
+            maxIndex = j
+
+    return sum(groups[minIndex])
+                    
+
+
+def main():
+    N = int(input())
+    K = int(input())
+
+    weights = []
+    for i in range(0, N):
+        weights.append(int(input()))
+
+    weights.sort(reverse=True)
+    
+    print(raft(K, weights))
+
+
+main()
