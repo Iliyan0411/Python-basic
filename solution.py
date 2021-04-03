a434ae36057d7fe43fdf7a11bc027ecb91dcc70e
Iliyan0411 <56543504+Iliyan0411@users.noreply.github.com>
2021-04-03 14:16:37 +0300

    -

--- solution.py
+++ solution.py
@@ -1,37 +1,31 @@
+def all_transported(arr):
+    for i in range(0, len(arr)):
+        if arr[i] == False:
+            return False
+    return True
 
 def raft(K, weights):
-    min = max(weights)
+    min = weights[0]
 
-    groups = []
-    for i in range(0, K):
-        groups.append([])
+    transported = []
+    for i in range(0, len(weights)):
+        transported.append(False)
 
-    gc = 0
-    i = 0
-    while i < len(weights):
-        if weights[i] + sum(groups[gc]) <= min:
-            groups[gc].append(weights[i])
-            i += 1
-        else:
-            if gc < K - 1:
-                gc += 1
-            else:
-                minIndex = 0
-                for j in range(1, K):
-                    if sum(groups[j]) < sum(groups[minIndex]):
-                        minIndex = j
-                
-                groups[minIndex].append(weights[i])
-                min = sum(groups[minIndex])
-                gc = 0
-                i += 1
+    while True:
+        for t in range(0, K):
+            sum = 0
+            for i in range(0, len(weights)):
+                if sum + weights[i] <= min and transported[i] == False:
+                    sum += weights[i]
+                    transported[i] = True
+            
+        if all_transported(transported):
+            return min
+        
+        min += 1
+        for j in range(0, len(transported)):
+            transported[j] = False
 
-    maxIndex = 0
-    for j in range(1, K):
-        if sum(groups[j]) > sum(groups[maxIndex]):
-            maxIndex = j
-
-    return sum(groups[minIndex])
                     
 
 
